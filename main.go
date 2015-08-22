b86da243e11fe0e1673146bb600187b6518e2ad9
Mark Petrovic <mark.petrovic@xoom.com>
2015-08-22 05:22:39 -0700

    ignore binaries

--- main.go
+++ main.go
@@ -10,31 +10,34 @@ import (
 )
 
 var (
-	root = flag.String("root-dir", ".", "root path")
+	root     = flag.String("root-dir", ".", "root path")
+	maxDepth = flag.Int("max-depth", 1, "max depth")
 )
 
 func main() {
 	flag.Parse()
-	if err := os.Chdir(*root); err != nil {
-		log.Fatalf("%v\n", err)
-	}
 	files := make([]string, 0)
 	markFn := func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() && strings.Count(path, "/") > 1 {
-			return filepath.SkipDir
-		}
 		if err != nil {
 			return err
 		}
-		if info.Name() == "config.xml" {
+		if info.IsDir() && strings.Count(path, "/") > *maxDepth-1 {
+			return filepath.SkipDir
+		}
+		if info.Mode().IsRegular() && info.Name() == "config.xml" {
 			files = append(files, path)
 		}
 		return nil
 	}
+
+	if err := os.Chdir(*root); err != nil {
+		log.Fatal(err)
+	}
+
 	err := filepath.Walk(".", markFn)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Printf("files: %d\n", len(files))
-	fmt.Printf("files: %v\n", (files))
 }
