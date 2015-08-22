1c73296437e479860e4af46f5f3a85f83d7dba3a
Mark Petrovic <mark.petrovic@xoom.com>
2015-08-22 05:24:56 -0700

    add filename option

--- main.go
+++ main.go
@@ -10,12 +10,17 @@ import (
 )
 
 var (
+	fileName = flag.String("filename", "", "file to search for")
 	root     = flag.String("root-dir", ".", "root path")
 	maxDepth = flag.Int("max-depth", 1, "max depth")
 )
 
 func main() {
 	flag.Parse()
+	if *fileName == "" {
+		log.Fatalf("need a filename\n")
+	}
+
 	files := make([]string, 0)
 	markFn := func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -24,7 +29,7 @@ func main() {
 		if info.IsDir() && strings.Count(path, "/") > *maxDepth-1 {
 			return filepath.SkipDir
 		}
-		if info.Mode().IsRegular() && info.Name() == "config.xml" {
+		if info.Mode().IsRegular() && info.Name() == *fileName {
 			files = append(files, path)
 		}
 		return nil
